08439ff4be041361288674b1162f29f901cff08a
Paula Cano <paulacanoroman@gmail.com>
2023-11-19 12:35:05 +0100

    feat: DatesInterval DateIntervalRange by ANT

--- pages/pantallaANT/screenPlanesDeCompra/components/filters/filtersComponents/filterDatesAndState/datesInterval/DatesInterval.jsx
+++ pages/pantallaANT/screenPlanesDeCompra/components/filters/filtersComponents/filterDatesAndState/datesInterval/DatesInterval.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styleDatesInterval from './DatesInterval.module.css'
+import DateIntervalRangeANT from './DateIntervalRangeANT'
 
 const DatesInterval = () => {
   return (
     <div className={styleDatesInterval.box3}> 
-      DatesInterval</div>
+      <span className={styleDatesInterval.titleDatesInterval}>PC vigentes en este intervalo</span>
+      <DateIntervalRangeANT></DateIntervalRangeANT>
+      </div>
   )
 }
 
