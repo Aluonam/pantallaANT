f2f58e89d1d117a786a558307605914b03782789
Paula Cano <paulacanoroman@gmail.com>
2023-12-11 18:03:43 +0100

    feat: delete component pagination

--- pages/pantallaANT/screenPlanesDeCompra/components/filters/filtersComponents/filterButtonsClear/FilterButtonClear.jsx
+++ pages/pantallaANT/screenPlanesDeCompra/components/filters/filtersComponents/filterButtonsClear/FilterButtonClear.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styleFilterButtonClear from './FilterButtonClear.module.css'
 import ButtonShowMoreANT from './buttonShowMore/ButtonShowMoreANT'
 import ButtonsFilterAndClear from './buttonsFilterAndClear/ButtonsFilterAndClear'
